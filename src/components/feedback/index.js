d011f7fc572c7ef0edb2d24562f04632b43ad19f
yashjsalian23 <yashjsalian23@gmail.com>
2023-06-08 12:49:01 +0530

    Added numbers in slider

--- src/components/feedback/index.js
+++ src/components/feedback/index.js
@@ -160,6 +160,53 @@ const FeedBack = () => {
     }
   };
 
+  const marks = [
+    {
+      value: 0,
+      label: "0",
+    },
+    {
+      value: 1,
+      label: "1",
+    },
+    {
+      value: 2,
+      label: "2",
+    },
+    {
+      value: 3,
+      label: "3",
+    },
+    {
+      value: 4,
+      label: "4",
+    },
+    {
+      value: 5,
+      label: "5",
+    },
+    {
+      value: 6,
+      label: "6",
+    },
+    {
+      value: 7,
+      label: "7",
+    },
+    {
+      value: 8,
+      label: "8",
+    },
+    {
+      value: 9,
+      label: "9",
+    },
+    {
+      value: 10,
+      label: "10",
+    },
+  ];
+
   return (
     <>
       {pageType === "START" ? (
@@ -264,6 +311,7 @@ const FeedBack = () => {
                     marginTop: 10,
                   }}
                   value={currentRating}
+                  marks={marks}
                   onChange={(e) => setCurrentRating(e.target.value)}
                 />
               </div>
